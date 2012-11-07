08508c47ec5c884c16bf7a658d3958e9d1c7f3c3
Emil Folino Nielsen <emilfolino@gmail.com>
2012-11-07 21:39:36 +0100

    When a social network is added the logo is removed from edit user registration page

--- app/models/authentication.rb
+++ app/models/authentication.rb
@@ -1,4 +1,17 @@
 class Authentication < ActiveRecord::Base
   belongs_to :user
-  attr_accessible :provider, :token, :uid
+  attr_accessible :provider, :token, :uid, :user_id
+  
+  def self.authenticated?(user_id, provider)
+    authentication = Authentication.where(:user_id => user_id, :provider => provider)
+    if authentication.nil?
+      return false
+    else
+      return true
+    end
+  end
+  
+  def self.number_of_authentications(user)
+    return Authentication.where(:user_id => user).count
+  end
 end
