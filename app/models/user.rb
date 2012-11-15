0e5ebfdfa9a946fd7cd044676f647b93f653b7b6
Anders Konring Olesen <anders.konring@gmail.com>
2012-11-15 20:11:41 +0100

    changes to config files to allow email sending

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :omniauthable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :group_id, :groups_attributes  
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -61,7 +61,7 @@ Ourworkouts::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
 
-  config.action_mailer.default_url_options = { :host => 'http://localhost:3000/' }
+  config.action_mailer.default_url_options = { :host => 'ourworkouts.herokuapp.com' }
   # ActionMailer Config
   # Setup for production - deliveries, no errors raised
   config.action_mailer.delivery_method = :smtp
@@ -72,7 +72,7 @@ Ourworkouts::Application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: 587,
-    domain: "example.com",
+    domain: "ourworkouts.herokuapp.com",
     authentication: "plain",
     enable_starttls_auto: true,
     user_name: "olesennielsen@gmail.com",
